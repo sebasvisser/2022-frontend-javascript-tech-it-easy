26844a1293d90b645eb2d91e2adc86255c7b645c
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-01 21:59:58 +0100

    start met 3b

--- opdracht3.js
+++ opdracht3.js
@@ -1,6 +1,5 @@
 // Opdracht 3 - Array methoden en functies
 // Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
-// Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
 
 const listItem = document.getElementById("tv-brands");
 inventory.map((tv) => {
@@ -10,3 +9,6 @@ inventory.map((tv) => {
     </li>
     `);
 });
+
+// Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
+// hoi
