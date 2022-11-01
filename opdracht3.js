4f223f3b2b4304e15617cfff383eea08ea4b8d13
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-01 22:13:23 +0100

    opdr 3a commented ivm dubbel

--- opdracht3.js
+++ opdracht3.js
@@ -1,6 +1,7 @@
 // Opdracht 3 - Array methoden en functies
 // Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
 
+// code commented ivm dubbele lijst door opdr 3b
 // const listItem = document.getElementById("tv-brands");
 // inventory.map((tv) => {
 //   return (listItem.innerHTML += `
@@ -12,7 +13,7 @@
 
 // Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
 
-function makeBrandList(tv) {
+function makeBrandList(inventory) {
   const listItem = document.getElementById("tv-brands");
   inventory.map((tv) => {
     return (listItem.innerHTML += `
@@ -22,5 +23,4 @@ function makeBrandList(tv) {
     `);
   });
 }
-
 makeBrandList(inventory);
