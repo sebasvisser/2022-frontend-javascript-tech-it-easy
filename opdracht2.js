069754f798c09605d11d63818aea6d8892168a47
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-01 20:48:13 +0100

    opdrach 2b, 2c en 2d gemaakt en alles netjes gemaakt

--- opdracht2.js
+++ opdracht2.js
@@ -7,14 +7,36 @@
 // Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
 //     Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
 
-//     Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+// Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
 console.log("Opdracht 2a:");
 let totalSoldTvs = 0;
 const soldTvs = inventory.map((tv) => tv.sold);
-soldTvs.forEach(myFunction);
 
-function myFunction(item) {
+soldTvs.forEach(countSold);
+
+function countSold(item) {
   totalSoldTvs += item;
 }
 
 console.log({ totalSoldTvs });
+
+// Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+console.log("Opdracht 2b: is zichtbaar op de rendered pagina");
+document.getElementById("sold-tvs").innerHTML = ` ${totalSoldTvs} `;
+
+//     Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+console.log("Opdracht 2c:");
+let totalBoughtTvs = 0;
+const boughtTvs = inventory.map((tv) => tv.originalStock);
+
+boughtTvs.forEach(countStock);
+
+function countStock(item) {
+  totalBoughtTvs += item;
+}
+
+console.log({ totalBoughtTvs });
+
+// Opdracht 2b: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+console.log("Opdracht 2d: is zichtbaar op de rendered pagina");
+document.getElementById("stock-tvs").innerHTML = ` ${totalBoughtTvs} `;
